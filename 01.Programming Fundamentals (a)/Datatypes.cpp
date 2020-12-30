cd716e7e7e7420aa9c94187d4d22750e7abcb976
vamshiearth <vamshiearth@gmail.com>
2020-12-30 12:28:56 +0530

    Updating Data types

---
+++ 01.Programming Fundamentals (a)/Datatypes.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  //Primitive data types
+
+  int a=12;
+  cout<<"Size of int : "<<sizeof(a)<<endl;
+
+  float b;
+  cout<<"Size of float : "<<sizeof(b)<<endl;
+
+  char c;
+  cout<<"Size of char : "<<sizeof(c)<<endl;
+
+  bool d;
+  cout<<"Size of bool : "<<sizeof(bool)<<endl;
+
+  //Type modifiers
+
+  long int e;
+  cout<<"Size of long int : "<<sizeof(e)<<endl;
+
+  short int f;
+  cout<<"Size of short int : "<<sizeof(f)<<endl;
+
+  signed int g;
+  cout<<"Size of signed int :"<<sizeof(g)<<endl;
+
+  unsigned int h;
+  cout<<"size of unsigned int : "<<sizeof(h)<<endl;
+
+
+  return 0;
+}
