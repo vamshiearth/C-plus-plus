5434c7f6b45bc8f8ef4786c25b61b96184699601
vamshiearth <vamshiearth@gmail.com>
2020-12-30 19:16:56 +0530

    Updating files

--- 01.Programming Fundamentals (a)/If_else.cpp
+++ 01.Programming Fundamentals (a)/If_else.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int a,b,c;
-  cout<<""  
+
+  int a,b,c,flag=0;
+
+  cout<<"Enter any three number\n";
+
+  cout<<"Enter number 1 : ";
+  cin>>a;
+  cout<<"Enter number 2 : ";
+  cin>>b;
+  cout<<"Enter number 3 : ";
+  cin>>c;
+  if(a>b){
+    if(a>c){
+      cout<<a<<" is the greater number";
+      flag=1;
+    }
+  }
+  if(b>a){
+    if(b>c){
+      cout<<b<<" is greater number";
+      flag=1;
+    }
+  }
+  if(flag=0){
+    cout<<c<<" is greater number";
+  }
   return 0;
 }
--- 01.Programming Fundamentals (a)/Loops.cpp
+++ 01.Programming Fundamentals (a)/Loops.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-  cout<<"Hello World!";
+  int arr[10],i;
+  cout<<"Enter any ten number\n";
+
+  for(i=0;i<10;i++){
+    cout<<"Element "<<i+1<<" = ";
+    cin>>arr[i];
+  }
+  int sum=0;
+  for(i=0;i<10;i++){
+    sum=sum+arr[i];
+  }
+  cout<<"Sum is "<<sum<<endl;
     return 0;
 }
