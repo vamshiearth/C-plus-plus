242d0b554d96f80511d1fba69999f8f024a10b02
vamshiearth <vamshiearth@gmail.com>
2020-12-30 13:06:46 +0530

    Adding 2nd file in 1

---
+++ 01.Programming Fundamentals (a)/Input_Output.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int amount1;
+    cout<<"Enter amount 1 : ";
+    cin>>amount1;
+    cout<<"Enter amount 2 : ";
+    int amount2;
+    cin>>amount2;
+
+    int sum=amount1+amount2;
+
+    cout<<"sum\n";
+
+    cout<<sum;
+
+    return 0;
+}
